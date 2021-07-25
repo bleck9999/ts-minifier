5e9d551cd496b374ecdd2fcc25ab31bb2552273c
bleck9999 <55853712+bleck9999@users.noreply.github.com>
2021-07-25 11:30:49 +0100

    fix whitespace stripping so that it doesn't break when a newline is used in place of a space

--- main.py
+++ main.py
@@ -2,55 +2,64 @@ import argparse
 
 
 def minify(script: str):
-    # currently ts does not seem to allow ''
+    # currently ts does not seem to allow 's to mark a quote
     # (https://github.com/suchmememanyskill/TegraExplorer/blob/tsv3/source/script/parser.c#L173)
     # im fine with that, it makes doing this a lot easier
-    minified = ""
     strings = script.split(sep='"')
     part = 0
+    requires = ""
+    mcode = ""
     while part < len(strings):
-        # done: clear all comments that don't have a REQUIRE
-        # done: strip whitespace
-        # TODO: shrink user defined names
+        # maybe in future it'll shrink user defined names
+        # dont hold out hope for that because `a.files.foreach("b") {println(b)}` is valid syntax
+        # and i dont have the skill or patience to deal with that
+        # though a suggestion that should be easier is aliasing functions that are used multiple times
+        # like in firmwaredump.te
 
         # in theory all the even numbered indexes should be outside quotes, so we ignore any parts with an odd index
         if part % 2 == 1:
-            minified += f'"{strings[part]}"'
-
+            mcode += f'"{strings[part]}"'
         else:
             for line in strings[part].split(sep='\n'):
                 if '#' in line:
                     if "REQUIRE" in line:
-                        minified += line + '\n'  # leave REQUIREs unmodified
+                        requires += line + '\n'  # leave REQUIREs unmodified
                         # comments are terminated by a newline so we need to add one back in
                     else:
                         # the comment is just a comment and can be ignored
                         pass
                 else:
-                    line = line.replace('\t', '')
-                    # tsv3 is still an absolute nightmare
-                    # so spaces have a couple edge cases
-                    # 1. the - operator which requires space between the right operand
-                    # yeah that's right only the right one
-                    # thanks meme
-                    # 2. between 2 letters
-                    index = 0
-                    newline = list(line)
-                    while index < (len(line)-4):
-                        sec = line[index:index+3]
-                        if sec[1] == ' ':
-                            if sec[0].isalpha() and sec[2].isalpha():
-                                pass
-                            elif sec[0] == '-' and sec[2].isnumeric():
-                                pass
-                            else:
-                                newline[index+1] = ''
-                        index += 1
-                    minified += ''.join(newline)
+                    mcode += line.replace('\t', '') + ' '
 
         part += 1
 
-    return minified
+    # tsv3 is still an absolute nightmare
+    # so spaces have a couple edge cases
+    # 1. the - operator which requires space between the right operand
+    # yeah that's right only the right one
+    # thanks meme
+    # 2. between 2 letters
+    inquote = False
+    mmcode = ""
+    index = 0
+    newline = list(mcode)
+    while index < (len(mcode)-3):
+        sec = mcode[index:index+3]
+        if not inquote and sec[1] == '"':
+            inquote = True
+        elif inquote and sec[1] == '"':
+            inquote = False
+        if (sec[1] == ' ') and not inquote:
+            if sec[0].isalpha() and sec[2].isalpha():
+                pass
+            elif sec[0] == '-' and sec[2].isnumeric():
+                pass
+            else:
+                newline[index+1] = ''
+        index += 1
+    mmcode += ''.join(newline)
+
+    return requires + mmcode.strip()
 
 
 if __name__ == '__main__':
